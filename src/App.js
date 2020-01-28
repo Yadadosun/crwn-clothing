ab4d29c7ac7f8f6d1ee26ca1e467f525184ff549
Dosun Kwon <dosunk@gmail.com>
2020-01-28 11:00:06 -0800

    shop page setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
-import HomePage from "../src/pages/homepage/homepage";
+import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
 import { Switch, Route } from "react-router-dom";
-import "./App.css";
 
-const HatsPage = () => {
-  return <div>Hi, HATS!</div>;
-};
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,243 @@
+const SHOP_DATA = [{
+        id: 1,
+        title: 'Hats',
+        routeName: 'hats',
+        items: [{
+                id: 1,
+                name: 'Brown Brim',
+                imageUrl: require('./assets/hats/brown-brim.png'),
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Blue Beanie',
+                imageUrl: require('./assets/hats/blue-beanie.png'),
+                price: 18
+            },
+            {
+                id: 3,
+                name: 'Brown Cowboy',
+                imageUrl: require('./assets/hats/brown-cowboy.png'),
+                price: 35
+            },
+            {
+                id: 4,
+                name: 'Grey Brim',
+                imageUrl: require('./assets/hats/grey-brim.png'),
+                price: 25
+            },
+            {
+                id: 5,
+                name: 'Green Beanie',
+                imageUrl: require('./assets/hats/green-beanie.png'),
+                price: 18
+            },
+            {
+                id: 6,
+                name: 'Palm Tree Cap',
+                imageUrl: require('./assets/hats/palm-tree-cap.png'),
+                price: 14
+            },
+            {
+                id: 7,
+                name: 'Red Beanie',
+                imageUrl: require('./assets/hats/red-beanie.png'),
+                price: 18
+            },
+            {
+                id: 8,
+                name: 'Wolf Cap',
+                imageUrl: require('./assets/hats/wolf-cap.png'),
+                price: 14
+            },
+            {
+                id: 9,
+                name: 'Blue Snapback',
+                imageUrl: require('./assets/hats/blue-snapback.png'),
+                price: 16
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: 'Sneakers',
+        routeName: 'sneakers',
+        items: [{
+                id: 10,
+                name: 'Adidas NMD',
+                imageUrl: require('./assets/sneakers/adidas-nmd.png'),
+                price: 220
+            },
+            {
+                id: 11,
+                name: 'Adidas Yeezy',
+                imageUrl: require('./assets/sneakers/yeezy.png'),
+                price: 280
+            },
+            {
+                id: 12,
+                name: 'Black Converse',
+                imageUrl: require('./assets/sneakers/black-converse.png'),
+                price: 110
+            },
+            {
+                id: 13,
+                name: 'Nike White AirForce',
+                imageUrl: require('./assets/sneakers/white-nike-high-tops.png'),
+                price: 160
+            },
+            {
+                id: 14,
+                name: 'Nike Red High Tops',
+                imageUrl: require('./assets/sneakers/nikes-red.png'),
+                price: 160
+            },
+            {
+                id: 15,
+                name: 'Nike Brown High Tops',
+                imageUrl: require('./assets/sneakers/nike-brown.png'),
+                price: 160
+            },
+            {
+                id: 16,
+                name: 'Air Jordan Limited',
+                imageUrl: require('./assets/sneakers/nike-funky.png'),
+                price: 190
+            },
+            {
+                id: 17,
+                name: 'Timberlands',
+                imageUrl: require('./assets/sneakers/timberlands.png'),
+                price: 200
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: 'Jackets',
+        routeName: 'jackets',
+        items: [{
+                id: 18,
+                name: 'Black Jean Shearling',
+                imageUrl: require('./assets/jackets/black-shearling.png'),
+                price: 125
+            },
+            {
+                id: 19,
+                name: 'Blue Jean Jacket',
+                imageUrl: require('./assets/jackets/blue-jean-jacket.png'),
+                price: 90
+            },
+            {
+                id: 20,
+                name: 'Grey Jean Jacket',
+                imageUrl: require('./assets/jackets/grey-jean-jacket.png'),
+                price: 90
+            },
+            {
+                id: 21,
+                name: 'Brown Shearling',
+                imageUrl: require('./assets/jackets/brown-shearling.png'),
+                price: 165
+            },
+            {
+                id: 22,
+                name: 'Tan Trench',
+                imageUrl: require('./assets/jackets/brown-trench.png'),
+                price: 185
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: 'Womens',
+        routeName: 'womens',
+        items: [{
+                id: 23,
+                name: 'Blue Tanktop',
+                imageUrl: require('./assets/womens/blue-tank.png'),
+                price: 25
+            },
+            {
+                id: 24,
+                name: 'Floral Blouse',
+                imageUrl: require('./assets/womens/floral-blouse.png'),
+                price: 20
+            },
+            {
+                id: 25,
+                name: 'Floral Dress',
+                imageUrl: require('./assets/womens/floral-skirt.png'),
+                price: 80
+            },
+            {
+                id: 26,
+                name: 'Red Dots Dress',
+                imageUrl: require('./assets/womens/red-polka-dot-dress.png'),
+                price: 80
+            },
+            {
+                id: 27,
+                name: 'Striped Sweater',
+                imageUrl: require('./assets/womens/striped-sweater.png'),
+                price: 45
+            },
+            {
+                id: 28,
+                name: 'Yellow Track Suit',
+                imageUrl: require('./assets/womens/yellow-track-suit.png'),
+                price: 135
+            },
+            {
+                id: 29,
+                name: 'White Blouse',
+                imageUrl: require('./assets/womens/white-vest.png'),
+                price: 20
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: 'Mens',
+        routeName: 'mens',
+        items: [{
+                id: 30,
+                name: 'Camo Down Vest',
+                imageUrl: require('./assets/mens/camo-vest.png'),
+                price: 325
+            },
+            {
+                id: 31,
+                name: 'Floral T-shirt',
+                imageUrl: require('./assets/mens/floral-shirt.png'),
+                price: 20
+            },
+            {
+                id: 32,
+                name: 'Black & White Longsleeve',
+                imageUrl: require('./assets/mens/long-sleeve.png'),
+                price: 25
+            },
+            {
+                id: 33,
+                name: 'Pink T-shirt',
+                imageUrl: require('./assets/mens/pink-shirt.png'),
+                price: 25
+            },
+            {
+                id: 34,
+                name: 'Jean Long Sleeve',
+                imageUrl: require('./assets/mens/roll-up-jean-shirt.png'),
+                price: 40
+            },
+            {
+                id: 35,
+                name: 'Burgundy T-shirt',
+                imageUrl: require('./assets/mens/polka-dot-shirt.png'),
+                price: 25
+            }
+        ]
+    }
+];
+
+export default SHOP_DATA;
